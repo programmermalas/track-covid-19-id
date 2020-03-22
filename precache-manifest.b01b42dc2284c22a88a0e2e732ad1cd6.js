bedbf7f118f182890079592a1395d438310c6202
Irfan Hadian <irfan.hadian10@gmail.com>
2020-03-22 22:14:14 +0700

    Updates

---
+++ precache-manifest.b01b42dc2284c22a88a0e2e732ad1cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d79645f95c2195b061905839f5ba26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7906e6ddb3eb01d51bb9",
+    "url": "/static/css/main.7ba92998.chunk.css"
+  },
+  {
+    "revision": "879aa62ec39c141f1446",
+    "url": "/static/js/2.bc417313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc417313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7906e6ddb3eb01d51bb9",
+    "url": "/static/js/main.4edb474f.chunk.js"
+  },
+  {
+    "revision": "4e605519eeea7c34de33",
+    "url": "/static/js/runtime-main.6ce7dcde.js"
+  }
+]);
